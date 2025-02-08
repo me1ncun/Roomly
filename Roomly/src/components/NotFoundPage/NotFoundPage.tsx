91eac787cb461eb3bdabaee49009f6687fc4ab61
Artem Puzenko <a.puzenko2004@gmail.com>
2025-02-08 17:12:09 +0200

    add frontend structure

---
+++ Roomly/src/components/NotFoundPage/NotFoundPage.tsx
@@ -0,0 +1,9 @@
+export const NotFoundPage = () => (
+  <div className="section">
+    <div className="container">
+      <br />
+      <br />
+      <h1 className="title">Page not found</h1>
+    </div>
+  </div>
+);
