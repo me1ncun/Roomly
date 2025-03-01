40f8d585f52c54a95b77a6b716a0b07230210c33
Denys Shapovalov <desha@smart-it.com>
2025-03-01 23:17:23 +0200

    Added CORS to API Gateway

--- Web/Roomly.ApiGateway/Program.cs
+++ Web/Roomly.ApiGateway/Program.cs
@@ -3,10 +3,23 @@ using Ocelot.Middleware;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowSpecificOrigin", builder =>
+    {
+        builder.WithOrigins("http://localhost:7000")
+            .AllowAnyHeader()
+            .AllowAnyMethod()
+            .AllowCredentials();
+    });
+});
+
 builder.Configuration.AddJsonFile("ocelot.json", optional: false, reloadOnChange: true);
 builder.Services.AddOcelot(builder.Configuration);
 
 var app = builder.Build();
+
+app.UseCors("AllowSpecificOrigin");
 
 await app.UseOcelot();
 
